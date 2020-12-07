3e9f363ba83c9916286a0f72a2c3118f2422f7e2
Farrukh Sadykov <farrukhsadykov@gmail.com>
2020-12-07 14:42:15 -0600

    Cant have a password

--- module/postgress_users.tf
+++ module/postgress_users.tf
@@ -2,6 +2,5 @@ resource "postgresql_role" "my_role" {
   count = length(var.user_names)
   name     = var.user_names[count.index]
   login    = true
-  password = "mypass"
   roles    = ["rds_iam"]
 }
